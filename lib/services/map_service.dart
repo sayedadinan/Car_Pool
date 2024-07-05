306bc87b22f592cf6280fe9bd4edcbd658657e21
sayed adinan <147618795+sayedadinan@users.noreply.github.com>
2024-07-05 20:25:11 +0530

    added map added and hided sensitive datas

--- lib/services/map_service.dart
+++ lib/services/map_service.dart
@@ -1,18 +1,18 @@
-import 'package:dio/dio.dart' as dio;
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
+// import 'package:dio/dio.dart' as dio;
+// import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
-Future<List<SearchInfo>> addressSuggestion(String searchText,
-    {int limitInformation = 5}) async {
-  dio.Response response = await dio.Dio().get(
-    "https://photon.komoot.io/api/",
-    queryParameters: {
-      "q": searchText,
-      "limit": limitInformation == 0 ? "" : "$limitInformation"
-    },
-  );
-  final json = response.data;
+// Future<List<SearchInfo>> addressSuggestion(String searchText,
+//     {int limitInformation = 5}) async {
+//   dio.Response response = await dio.Dio().get(
+//     "https://photon.komoot.io/api/",
+//     queryParameters: {
+//       "q": searchText,
+//       "limit": limitInformation == 0 ? "" : "$limitInformation"
+//     },
+//   );
+//   final json = response.data;
 
-  return (json["features"] as List)
-      .map((d) => SearchInfo.fromPhotonAPI(d))
-      .toList();
-}
+//   return (json["features"] as List)
+//       .map((d) => SearchInfo.fromPhotonAPI(d))
+//       .toList();
+// }
